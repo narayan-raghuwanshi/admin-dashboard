e081737d7b25b30fb401308419edad307077e536
Narayan Raghuwanshi <narayan.raghu26@gmail.com>
2024-09-27 11:53:35 +0530

    Added github logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ProjectResources from '@/components/ProjectResources'
 import Table from '@/components/Table'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
+      <ProjectResources />
       <div className='flex flex-col items-center mt-20'>
         <h2 className='mb-10 text-4xl sm:text-5xl font-semibold'>Admin <span className='text-blue-600'>dashboard.</span></h2>
       </div>
