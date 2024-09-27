83797a0bdb36d5ef3466cd5fff43af56fe6c6149
Narayan <narayan.raghu26@gmail.com>
2024-09-27 11:56:03 +0530

    Update ProjectResources.tsx

--- src/components/ProjectResources.tsx
+++ src/components/ProjectResources.tsx
@@ -4,7 +4,7 @@ import { FaGithub } from 'react-icons/fa'
 
 const ProjectResources = () => {
     return (
-        <Link href="https://github.com/narayan-raghuwanshi/managewise-replica" target='_blank' className="bg-white rounded-full border-4 border-gray-800 fixed bottom-10 right-10">
+        <Link href="https://github.com/narayan-raghuwanshi/admin-dashboard" target='_blank' className="bg-white rounded-full border-4 border-gray-800 fixed bottom-10 right-10">
             <FaGithub size={40} />
         </Link>
     )
